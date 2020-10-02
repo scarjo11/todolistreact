71042a12ab54b6b7d4b366913c3540db10cd2be5
scarjo11 <sezzle-wave@live.fr>
2020-10-02 02:12:54 +0200

    Updates

---
+++ precache-manifest.f810222c4747c9de64d047e86f3ae566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3791768e5bd663848dfe6135c90fb2f7",
+    "url": "/todolistreact/index.html"
+  },
+  {
+    "revision": "15a90b7a400e782c09c8",
+    "url": "/todolistreact/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "15a90b7a400e782c09c8",
+    "url": "/todolistreact/static/js/2.406f9152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolistreact/static/js/2.406f9152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68343852447ad541b64f",
+    "url": "/todolistreact/static/js/main.030d088d.chunk.js"
+  },
+  {
+    "revision": "33b96d4d443a5b377bd5",
+    "url": "/todolistreact/static/js/runtime-main.686b3a12.js"
+  }
+]);
